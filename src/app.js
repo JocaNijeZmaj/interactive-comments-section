5c7d68136442460decd3876ff30e28724ac30ddc
JocaNijeZmaj <100790659+JocaNijeZmaj@users.noreply.github.com>
2022-06-21 00:28:04 +0200

    21.6

--- src/app.js
+++ src/app.js
@@ -4,3 +4,15 @@
 // 3: brisanje komentara
 // treba da ima model na kome ce da pise are yous sure...
 // 4: editovanje komentara
+
+
+const likeBtn = document.querySelectorAll(".like");
+const dislikeBtn = document.querySelectorAll(".dislike");
+const reply = document.querySelectorAll(".show_reply");
+const send_btn = document.querySelectorAll(".send_btn");
+const input_area = document.querySelectorAll(".input_area");
+const show_delete = document.querySelectorAll(".show_delete");
+const show_edit = document.querySelectorAll(".show_edit");
+const comment_date = document.querySelectorAll(".comment_date");
+
+
